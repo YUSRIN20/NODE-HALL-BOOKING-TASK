46f56fa84056c6bdd4c5242afbf1f7056087afe3
YUSRIN20 <massyusrin123@gmail.com>
2024-03-06 01:54:16 +0530

    added h1 tag

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const PORT  = 4000;
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.status(200).send(`<div><h1>Golden Oasis Retreat</h1></div>`)
+    res.status(200).send(`<div style="text-align: center; background-color:light-blue"><h1>Welcome to: Golden Oasis Retreat</h1></div>
+    <div><h1>Change the endpoint to <mark>/allroomdetails</mark> to get the All Room Details</h1></div>`)
 })
 app.use('/hallapi',hallRouter)
 
