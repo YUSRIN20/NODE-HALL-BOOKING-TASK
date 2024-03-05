4f399c109b2af1600086e11ee952235c0fe87609
YUSRIN20 <massyusrin123@gmail.com>
2024-03-06 02:10:27 +0530

    added some description for h1

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const PORT  = 4000;
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.status(200).send(`<div style="text-align: center; background-color:light-blue"><h1>Welcome to: Golden Oasis Retreat</h1></div>
-    <div><h1>Change the endpoint to <mark>/allroomdetails</mark> to get the All Room Details</h1></div>`)
+    res.status(200).send(`<div style="text-align: center; background-color:light-blue;"><h1>Welcome to: Golden Oasis Retreat Hall</h1></div>
+    <div  style="text-align: center>
+    <ul>
+    <li><h3><mark>Get:</mark> Use the endpoint to <mark>/hallapi/allroomdetails</mark> to get the All Room Details</h3></li>
+    <li><h3><mark>Post:</mark>Change the endpoint to <mark>/hallapi/createroom</mark> to Create the New Room</h3></li>
+    <li><h3><mark>Get:</mark>Change the endpoint to <mark>/hallapi/bookingroom</mark> to Booking New Room</h3></li>
+    <li><h3><mark>Get:</mark>Change the endpoint to <mark>/hallapi/bookedroomdata</mark> to get the Booked Room Data</h3></li>
+    <li><h3><mark>Get:</mark>Change the endpoint to <mark>/hallapi/customersbookeddata</mark> to get the Booked customers & Room Data</h3></li>
+    <li><h3><mark>Get:</mark>Change the endpoint to <mark>/hallapi/customerbookingcount</mark> to get the Booked customers Booking Count & Room Data</h3></li>
+    </ul> 
+    </div>`)
 })
 app.use('/hallapi',hallRouter)
 
